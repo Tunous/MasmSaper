53c3a0dfe2edfe94de0bee0022e85ef801ef5b82
Tunous <lukus178@gmail.com>
2016-11-27 16:20:25 +0100

    Update makefile

--- makeit.bat
+++ makeit.bat
@@ -1,33 +1,21 @@
 @echo off
 
-: If resources exist, build them
-if not exist rsrc.rc goto over1
-\masm32\bin\rc.exe /v rsrc.rc
-\masm32\bin\cvtres.exe /machine:ix86 rsrc.res
-:over1
-
 : Cleanup
 if exist "MasmSaper.obj" del "MasmSaper.obj"
 if exist "MasmSaper.exe" del "MasmSaper.exe"
 
 : Assmeble into OBJ file
-\masm32\bin\ml /c /coff "MasmSaper.asm"
+\masm32\bin\ml /coff /c "MasmSaper.asm"
 if errorlevel 1 goto errasm
 
-if not exist rsrc.obj goto nores
+: Build resources
+\masm32\bin\rc rsrc.rc
 
-: Link the main OBJ file with the resource OBJ file
-\masm32\bin\link.exe /SUBSYSTEM:WINDOWS "MasmSaper.obj" "rsrc.obj"
+: Build exe
+\masm32\bin\link /SUBSYSTEM:WINDOWS "MasmSaper.obj" "rsrc.res"
 if errorlevel 1 goto errlink
 dir "MasmSaper.*"
 goto TheEnd
-
-: Link the main OBJ file
-:nores
-  \masm32\bin\PoLink /SUBSYSTEM:WINDOWS "MasmSaper.obj"
-  if errorlevel 1 goto errlink
-  dir "MasmSaper.*"
-  goto TheEnd
 
 :errlink
   echo _
